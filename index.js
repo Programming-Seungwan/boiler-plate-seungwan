67089fefa2e3c2fb28d7142f1cdaa552c3c5c53c
Programming-Seungwan <swsj4480@gmail.com>
2023-10-01 17:53:28 +0900

    Feat: make register & login func

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const app = express();
 const { User } = require('./models/User');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const cookieParser = require('cookie-parser');
 // 클라이언트에서 오는 정보를 서버에서 분석해서 가져올 수 있도록 도와주는 미들웨어를 등록
 // applicatiopn/x-www-form-urlencoded 같은 데이터
 app.use(bodyParser.urlencoded({ extended: true }));
 // 이건 application/json을 가져올 수 있도록
 app.use(bodyParser.json());
+app.use(cookieParser());
 // key는 mongoDB의 connect와 관련된 것에 해당
 mongoose
   .connect(
@@ -30,6 +31,62 @@ app.post('/register', async (req, res) => {
     .save()
     .then(() => res.status(200).json({ success: true }))
     .catch((err) => res.json({ success: false, err }));
+});
+
+app.post('/login', async (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는지를 찾는다
+  try {
+    const foundUser = await User.findOne({ email: req.body.email });
+    foundUser.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: '비밀번호가 일치하지 않습니다',
+        });
+      }
+    });
+
+    foundUser.generateToken((err, user) => {
+      if (err) res.status(400).send(err);
+      res
+        .cookie('x_auth', user.token)
+        .status(200)
+        .json({ loginSuccess: true, userId: user._id });
+    });
+  } catch (err) {
+    return res.json({
+      loginSuccess: true,
+      message: '제공된 이메일에 해당하는 유저가 없습니다',
+    });
+  }
+
+  // User.findOne({ email: req.body.email }, (err, userInfo) => {
+  //   if (!userInfo) {
+  //     return res.json({
+  //       loginSuccess: false,
+  //       message: '제공된 이메일에 해당하는 유저가 없습니다',
+  //     });
+  //   }
+  //   // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+  //   // 비밀번호를 비교하는 메서드를 만든다
+  //   userInfo.comparePassword(req.body.password, (err, isMatch) => {
+  //     if (!isMatch) {
+  //       return res.json({
+  //         loginSuccess: false,
+  //         message: '비밀번호가 틀렸습니다',
+  //       });
+  //     }
+  //     // generateToken은 실패하면 에러만 가진 콜백을, 성공하면 에러는 null이고 토큰 정보를 가진 user을 가져옴
+  //     userInfo.generateToken((err, user) => {
+  //       if (err) return res.status(400).send(err);
+  //       // token을 쿠키 & 로컬 스토리지 등 여러 군데에 저장할 수 있음
+  //       res
+  //         .cookie('x_auth', user.token)
+  //         .status(200)
+  //         .json({ loginSuccess: true, userId: user._id });
+  //     });
+  //   });
+  // });
 });
 
 app.listen(3000, () => {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 // 암호화를 몇 자리로 시킬 것인지를 나타내는 것이 saltRounds이다
 const saltRounds = 10;
 const userSchema = mongoose.Schema({
@@ -54,6 +55,27 @@ userSchema.pre('save', function (next) {
     next();
   }
 });
+
+// 스키마에 methods로 메서드를 만들어주면 모델 인스턴스에서 사용할 수 있음
+// index.js에서 findOne()의 결과물은 스키마로 만든 모델의 인스턴스니까 아래 메서드를 쓸 수 있음
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  const user = this;
+  // jsonwebtoken을 이용해서 token을 생성하기
+  // id와 서버 단의 정보(문자열)을 이용해서 토큰을 만들어냄
+  const token = jwt.sign(user._id.toHexString(), 'secretToken');
+  user.token = token;
+  user
+    .save()
+    .then(() => cb(null, user))
+    .catch((err) => cb(err));
+};
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
