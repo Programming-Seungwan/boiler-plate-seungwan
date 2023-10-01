8a8008add4f66698e1b073e3cf3551f55c860c8c
Programming-Seungwan <swsj4480@gmail.com>
2023-10-01 16:04:00 +0900

    Feat: encrypt password

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
+const { User } = require('./models/User');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// 클라이언트에서 오는 정보를 서버에서 분석해서 가져올 수 있도록 도와주는 미들웨어를 등록
+// applicatiopn/x-www-form-urlencoded 같은 데이터
+app.use(bodyParser.urlencoded({ extended: true }));
+// 이건 application/json을 가져올 수 있도록
+app.use(bodyParser.json());
 // key는 mongoDB의 connect와 관련된 것에 해당
 mongoose
   .connect(
@@ -13,6 +20,16 @@ mongoose
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
+});
+
+app.post('/register', async (req, res) => {
+  // 회원 가입 시, 필요한 정보들을 클라이언트에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다
+  const user = new User(req.body);
+  const result = await user
+    .save()
+    .then(() => res.status(200).json({ success: true }))
+    .catch((err) => res.json({ success: false, err }));
 });
 
 app.listen(3000, () => {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+// 암호화를 몇 자리로 시킬 것인지를 나타내는 것이 saltRounds이다
+const saltRounds = 10;
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -31,6 +33,27 @@ const userSchema = mongoose.Schema({
   },
 });
 
+// 저장하기 이전의 미들웨어에 해당함
+userSchema.pre('save', function (next) {
+  // 스키마를 가리키게 됨
+  const user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      // index.js 에서의 에러 핸들링 미들웨어로 빠지게 됨
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        // 저장될 user 스키마의 비밀번호를 만든 hash 값으로 교체해준다
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+module.exports = { User };
