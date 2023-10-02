02315a4f278caf62bae9ea3f269e349c388bf1a6
Programming-Seungwan <swsj4480@gmail.com>
2023-10-03 01:34:35 +0900

    Feat: make route func in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
 
 function App() {
-  return <div>AppAppAppApps</div>;
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path='/' element={<LandingPage />}></Route>
+          <Route exact path='/login' element={<LoginPage />}></Route>
+          <Route exact path='/dashboard' element={<RegisterPage />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
+
+// function Home() {
+//   return (
+//     <div>
+//       <h2>Home</h2>
+//     </div>
+//   );
+// }
+
+// function About() {
+//   return (
+//     <div>
+//       <h2>About 어바웃 페이지</h2>
+//     </div>
+//   );
+// }
+
+// function Dashboard() {
+//   return (
+//     <div>
+//       <h2>Dashboard</h2>
+//     </div>
+//   );
+// }
